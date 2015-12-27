60b919febc07d2f574452996136a31c5b36367b3
Thor Garcia <thor.fc21@gmail.com>
2015-12-27 19:15:37 -0200

    Add some stuff.

---
+++ README.md
@@ -0,0 +1,23 @@
+# Angry-Phrases
+
+This is a pretty non-sense NPM package made to get some random angry phrases. And yes, some phrases have bad words. Enjoy.  :smile:
+
+***
+
+## Install
+
+> npm install angry-phrases --save
+
+***
+
+## Usage
+
+``` js
+
+var angry = require('angry-phrase');
+
+var phrase = angry.getGreatPhrase();
+
+console.log("Inspiring Phrase: " + phrase);
+
+```
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 listPhrases =
-    ['You sucks', 'You deserve to die', 'Law should not be applied to you',
-    'Fuck off everything', 'I do not care', 'Burn in hell', 'More hate, please',
-    'Your mom does not like you', 'Your father does not like you',
-    'How about you die?', 'Fuck your religion, fuck your dreams, fuck you'];
+    ['You sucks.', 'You deserve to die.', 'Law should not be applied to you.',
+    'Fuck off everything.', 'I do not care.', 'Burn in hell.', 'More hate, please.',
+    'Your mom does not like you.', 'Your father does not like you.',
+    'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.'];
 
 exports.getGreatPhrase = function(){
     var phrase = listPhrases[Math.floor(Math.random()*listPhrases.length)];
