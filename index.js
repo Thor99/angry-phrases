42c005241ef90007a8bdbb7cd6bccb0d8b9a31e9
Thor Garcia <thor.fc21@gmail.com>
2015-12-27 19:39:22 -0200

    Fix bug.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ listPhrases =
     'Fuck off everything.', 'I do not care.', 'Burn in hell.', 'More hate, please.',
     'Your mom does not like you.', 'Your father does not like you.',
     'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.',
-    'Might be shit be you.', 'I will kill you.', 'Merry christmas and burn.'
+    'Might be shit be you.', 'I will kill you.', 'Merry christmas and burn.',
     'Fuck global warming', 'The world is my toilet.',
     'This is not my problem.'];
 
