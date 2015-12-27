fa34bba23ba165788c4433afe76216d50ed0e994
Thor Garcia <thor.fc21@gmail.com>
2015-12-27 19:36:30 -0200

    Add some phrases.

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 listPhrases =
-    ['You sucks.', 'You deserve to die.', 'Law should not be applied to you.',
+    ['You suck.', 'You deserve to die.', 'Law should not be applied to you.',
     'Fuck off everything.', 'I do not care.', 'Burn in hell.', 'More hate, please.',
     'Your mom does not like you.', 'Your father does not like you.',
-    'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.'];
+    'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.',
+    'Might be shit be you.', 'I will kill you.', 'Merry christmas and burn.'
+    'Fuck global warming', 'The world is my toilet.',
+    'This is not my problem.'];
 
 exports.getGreatPhrase = function(){
     var phrase = listPhrases[Math.floor(Math.random()*listPhrases.length)];
