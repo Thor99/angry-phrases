ba25df5e3db4d520c75c2663f73068ee70d56e17
Igor Antun <igorantunc@hotmail.com>
2018-10-02 15:18:32 -0300

    ANGER

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const listPhrases =
     'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.',
     'Might be shit to be you.', 'I will kill you.', 'Merry christmas and burn.',
     'Fuck global warming.', 'The world is my toilet.',
-    'This is not my problem.', 'You are a cunt.'];
+    'This is not my problem.', 'You are a cunt.', 'I will make you digest your own teeth',
+     'You are a fucking salboo', 'You smell like hot dog water'];
 
 module.exports = () => listPhrases[ 0| Math.random() * listPhrases.length];
