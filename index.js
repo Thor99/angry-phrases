43103bb7af3c255c2dbb9d1af8abba6a14728ef3
Muhammad Irsyad <irsyart@gmail.com>
2018-10-03 01:27:41 +0700

    Add another angry phrase, change array value placement

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 'use strict';
 
-const listPhrases = 
-    ['You suck.', 'You deserve to die.', 'Law should not be applied to you.',
-    'Fuck off everything.', 'I do not care.', 'Burn in hell.', 'More hate, please.',
-    'Your mom does not like you.', 'Your father does not like you.',
-    'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.',
-    'Might be shit to be you.', 'I will kill you.', 'Merry christmas and burn.',
-    'Fuck global warming.', 'The world is my toilet.',
-    'This is not my problem.', 'You are a cunt.'];
+const listPhrases = [
+    'You suck.',
+    'You deserve to die.',
+    'Law should not be applied to you.',
+    'Fuck off everything.',
+    'I do not care.',
+    'Burn in hell.',
+    'More hate, please.',
+    'Your mom does not like you.',
+    'Your father does not like you.',
+    'How about you die?',
+    'Fuck your religion, fuck your dreams, fuck you.',
+    'Might be shit to be you.',
+    'I will kill you.',
+    'Merry christmas and burn.',
+    'Fuck global warming.',
+    'The world is my toilet.',
+    'This is not my problem.',
+    'You are a cunt.',
+    'You moron!',
+    'I will kick you in the face.'
+];
 
 module.exports = () => listPhrases[ 0| Math.random() * listPhrases.length];
