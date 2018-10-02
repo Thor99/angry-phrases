c72fd29ce11ebfd8cf26bb69424f05c7e5ab9df0
Sidhya Tikku <33092447+sidhyatikku@users.noreply.github.com>
2018-10-03 00:00:51 +0530

    Added some burns

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const listPhrases =
     'How about you die?', 'Fuck your religion, fuck your dreams, fuck you.',
     'Might be shit to be you.', 'I will kill you.', 'Merry christmas and burn.',
     'Fuck global warming.', 'The world is my toilet.',
-    'This is not my problem.', 'You are a cunt.'];
+    'This is not my problem.', 'You are a cunt.', 'Imma whoop yo ass up.', 'You are a Prick.', 
+    'You are proof that evolution CAN go in reverse.', 'There is only one problem with your face, I can see it.',
+    'Bitch you are like Monday, nobody likes you.', 'The last time I saw something like you, I flushed it.',
+    'If I wanted to kill myself, I would climb up your ego and jump down to your IQ level.', 'How did you get here? Did someone leave your cage open?',
+    'I’d slap you but that will be animal abuse.'];
 
 module.exports = () => listPhrases[ 0| Math.random() * listPhrases.length];
